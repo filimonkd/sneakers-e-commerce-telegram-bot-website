1c1b6a804f8d48256080363080c54af61bda00be
filimonkd <filimonkd@gmail.com>
2023-05-10 10:55:54 +0300

    update cart

--- src/components/Cart/Cart.vue
+++ src/components/Cart/Cart.vue
@@ -6,37 +6,34 @@
 
     <div class="py-5 grid place-items-center gap-5">
 
-      <p v-if="setCartValue === null" class="font-bold text-xl">Your cart is empty.</p>
-      <button v-if="setCartValue === null" @click="showCart = !showCart"  class="bg-[#ff7d1a] px-4 py-2 text-white font-bold">
+      <p v-if="setCartValue === null && !this.$store.state.checkoutState" class="font-bold text-xl">Your cart is empty.</p>
+      <button v-if="setCartValue === null && !this.$store.state.checkoutState" @click="this.$store.state.showCart = !this.$store.state.showCart"
+        class="bg-[#ff7d1a] px-4 py-2 text-white font-bold">
         Close
       </button>
 
-      <!-- <div class="flex flex-col gap-4 items-center ">
-            <img
-              src="https://c.tenor.com/qM6fp_G04OwAAAAj/tick-tick-verified.gif"
-              alt="check"
-              class="h-[100px]"
-            />
-            <p class="font-bold text-xl ">Your order is on the way</p>
-          </div> -->
+      <div v-if="this.$store.state.checkoutState" class="flex flex-col gap-4 items-center ">
+        <img src="https://c.tenor.com/qM6fp_G04OwAAAAj/tick-tick-verified.gif" alt="check" class="h-[100px]" />
+        <p class="font-bold text-xl ">Your order is on the way</p>
+      </div>
 
 
-        <div v-if="setCartValue != null" class="flex items-center gap-x-3 justify-between w-[90%] ">
-          <img src="../../images/image-product-1.jpg" alt="shoe1" class="h-[60px] rounded-lg" />
-          <div class="font-medium text-gray-400 ">
-            <p>Fall Limited Edition Sneakers</p>
-            <p>
-              $125.00 x {{count}}:
-              <span class="font-bold text-black">
-                ${{setSneakersPrice}}
-              </span>
-            </p>
-          </div>
-          <img src='../../images/icon-delete.svg' alt="delete" @click="deleteCart()" class="h-[20px] cursor-pointer" />
+      <div v-if="setCartValue != null && !this.$store.state.checkoutState" class="flex items-center gap-x-3 justify-between w-[90%] ">
+        <img src="../../images/image-product-1.jpg" alt="shoe1" class="h-[60px] rounded-lg" />
+        <div class="font-medium text-gray-400 ">
+          <p>Fall Limited Edition Sneakers</p>
+          <p>
+            $125.00 x {{ count }}:
+            <span class="font-bold text-black">
+              ${{ setSneakersPrice }}
+            </span>
+          </p>
         </div>
-        <button v-if="setCartValue != null" class="checkoutBtn">
-          Checkout
-        </button>
+        <img src='../../images/icon-delete.svg' alt="delete" @click="deleteCart()" class="h-[20px] cursor-pointer" />
+      </div>
+      <button v-if="setCartValue != null && !this.$store.state.checkoutState" @click="checkoutCart()" class="checkoutBtn">
+        Checkout
+      </button>
     </div>
   </div>
 </template>
@@ -50,14 +47,14 @@ export default {
   name: "CartIcon",
   data() {
     return {
-      showCart:true
+
     };
   },
   methods: {
-    ...mapMutations(["deleteCart","showCartHandler"])
+    ...mapMutations(["deleteCart", "showCartHandler", "checkoutCart"])
   },
   computed: {
-    ...mapState(["setCartValue","setSneakersPrice","count"])
+    ...mapState(["setCartValue", "setSneakersPrice", "count"])
   }
 }
 </script>
